e4019e00a297dba9789968992b2aabb8c7e2f8fe
phaiemko123 <ducvu.q7@gmail.com>
2016-11-11 16:55:43 +0700

    continue config my app chat

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
 //http://stackoverflow.com/questions/10058226/send-response-to-all-clients-except-sender-socket-io
 var express = require('express');
+
 var app = express();
+
 var server = require('http').Server(app);
+
 var io = require('socket.io')(server);
-var bodyParser = require('body-parser');
+
 var port = process.env.PORT || 3000;
+
+//first run
+require('./lib/init')(app, io);
+
+var config  = require('./lib/config');
+
+//run config init
+config.init();
+
+var db      = require('./lib/vndb');
+
+//config run with promise
+db.usePromise = true;
+
+pool = db.pool = config.configDB();
+
+
 server.listen(port, function(){
     console.log('connected');
 });
 
-app.set('view engine', 'ejs');
-app.set('views','./views');
-app.use(express.static('public'));
-
-session = require('express-session');
-app.use(session({
-    secret: '2C44-4D44-WppQ38S',
-    resave: true,
-    saveUninitialized: true
-}));
-
-var pg = require('pg');
-
-var config = {
-    user: 'postgres', //env var: PGUSER
-    database: 'mychat', //env var: PGDATABASE
-    password: '123456', //env var: PGPASSWORD
-    host: 'localhost', // Server hosting the postgres database
-    port: 5432, //env var: PGPORT
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-};
-
-var pool = new pg.Pool(config);
+parser = config.configBodyParser();
 
 var onlineUser = [];
 
@@ -44,6 +41,7 @@ var listFriend = [
 ];
 
 io.on('connection',function(socket){
+
     console.log('Co nguoi ket noi : ' + socket.id);
 
     var index  = onlineUser.length;
@@ -81,8 +79,6 @@ io.on('connection',function(socket){
 });
 
 
-var urlEncode = bodyParser.urlencoded({ extended: false });
-
 app.get('/',function(req, res){
     if (!req.session.userInfo) {
         res.render('index');
@@ -97,28 +93,32 @@ app.get('/clear',function(req, res){
     res.send('ok');
 });
 
-app.post('/', urlEncode, function(req, res){
-    var post = req.body;
-    checkSignIn(post.username,post.password, function(err, result){
+//get data when user login
+app.post('/', parser, function(req, res){
 
-        if(result.rowCount == 1)
+    var post = req.body;
+
+    var arrQuery = {table : 'user',where : `"username" = '${post.username}' and "password" = '${post.password}'`};
+
+    db.sqlSelect(arrQuery).then(function(oResult, error){
+        if(oResult.total > 0)
         {
-            oRows = result.rows[0];
-            req.session.userInfo = {userId : oRows.id, userName :oRows.username, userMStatus : oRows.mstatus };
+            result = oResult.result[0];
+            req.session.userInfo = {userId : result.id, userName :result.username, userMStatus : result.mstatus };
             res.redirect('/chat');
         }
         else {
             res.send('Bad user/pass');
         }
+    }).catch(function(error) {
+
+        console.log('Error occurred! ', error);
+
     });
+
 });
 
 app.get('/chat', checkAuth, function(req, res){
-    res.send('if you are viewing this page it means you are logged in');
-    //res.render('chat');
-});
-
-app.get('/chat1', function(req, res){
     res.send('if you are viewing this page it means you are logged in');
     //res.render('chat');
 });
@@ -135,27 +135,6 @@ function infoUser(userId,userName){
     this.userName = userName;
 }
 
-function query(sql, cb){
-    pool.connect(function(err, client, done){
-        if(err){
-            done();
-            console.log('LOI KET NOI: ' + err);
-        }else{
-            client.query(sql, function(err, result){
-                done();
-                cb(err, result);
-            });
-        }
-    });
-}
-
-function checkSignIn(username, password, cb){
-    var sql = `SELECT * FROM "user" WHERE "username" = '${username}' AND
-  "password" = '${password}'`;
-    query(sql, function(err, result){
-        cb(err, result);
-    });
-}
 
 function checkAuth(req, res, next) {
     if (!req.session.userInfo) {
